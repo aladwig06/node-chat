5cfb4797747dae8d80e0af966106f1bed5ac8fd4
aladwig06 <andrew.ladwig@gmail.com>
2013-08-27 12:48:48 +0000

    initial commit

---
+++ chat.js
@@ -0,0 +1,29 @@
+    var app = require('http').createServer(handler);
+   
+    var stat = require('node-static');
+   
+var fileServer = new stat.Server('./');
+
+
+app.listen(8081);
+
+
+
+function handler (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response);
+    }).resume();
+}
+
+ var io = require('socket.io').listen(app);
+ io.set('log level', 1);
+
+io.sockets.on('connection', function (socket) {
+        console.log('New Connection');
+      socket.on('input', function (data){
+        socket.broadcast.emit('input', data);
+      console.log("Emitting: " + data.text);        
+      });
+    
+});
+
